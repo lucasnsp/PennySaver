dec532d999f7263cb62c5622414479c21d0d2a5d
Lucas Neves <lucas.pompeu02@hotmail.com>
2024-06-25 19:12:37 -0300

    Recents HeaderView functions,

--- PennySaver/PennySaver/View/TabViews/RecentsView.swift
+++ PennySaver/PennySaver/View/TabViews/RecentsView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct RecentsView: View {
+    // User Properties
+    @AppStorage("userName") private var userName: String = ""
     var body: some View {
         GeometryReader {
             let size = $0.size
@@ -30,17 +32,68 @@ struct RecentsView: View {
     // Header View
     @ViewBuilder
     func HeaderView(_ size: CGSize) -> some View {
-        VStack(alignment: .leading, spacing: 5, content: {
-            Text("Bem vindo!")
-                .font(.title.bold())
-        })
-        .hSpacing(.leading)
-        .background {
-            Rectangle()
-                .fill(.ultraThinMaterial)
-                .padding(.horizontal, -15)
-                .padding(.top, -(safeArea.top + 15))
+        HStack(spacing: 10) {
+            VStack(alignment: .leading, spacing: 5, content: {
+                Text("Bem-vindo!")
+                    .font(.title.bold())
+
+                if !userName.isEmpty {
+                    Text(userName)
+                        .font(.callout)
+                        .foregroundStyle(.gray)
+                }
+            })
+            .visualEffect { content, geometryProxy in
+                content
+                    .scaleEffect(headerScale(size, proxy: geometryProxy), anchor: .topLeading)
+            }
+
+
+            Spacer(minLength: 0)
+
+                .overlay(alignment: .trailing, content: {
+                    NavigationLink {
+
+                    } label: {
+                        Image(systemName: "plus")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.white)
+                            .frame(width: 45, height: 45)
+                            .background(appTint.gradient, in: .circle)
+                            .contentShape(.circle)
+                    }
+                })
         }
+        .padding(.bottom, userName.isEmpty ? 10 : 5)
+        .background {
+            VStack(spacing: 0) {
+                Rectangle()
+                    .fill(.ultraThinMaterial)
+
+                Divider()
+            }
+            .visualEffect { content, geometryProxy in
+                content
+                    .opacity(headerBGOpacity(geometryProxy))
+            }
+            .padding(.horizontal, -15)
+            .padding(.top, -(safeArea.top + 15))
+        }
+    }
+
+    func headerBGOpacity(_ proxy: GeometryProxy) -> CGFloat {
+        let minY = proxy.frame(in: .scrollView).minY + safeArea.top
+        return minY > 0 ? 0 : (-minY / 15)
+    }
+
+    func headerScale(_ size: CGSize, proxy: GeometryProxy) -> CGFloat {
+        let minY = proxy.frame(in: .scrollView).minY
+        let screenHeight = size.height
+
+        let progress = minY / screenHeight
+        let scale = (min(max(progress, 0 ), 1)) * 0.3
+        return 1 + scale
     }
 }
 
