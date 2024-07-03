6982a790257d3768b5995a56fac4140817c7dd82
Lucas Neves <lucas.pompeu02@hotmail.com>
2024-07-03 13:50:54 -0300

    Finishing chartsView and configurations for the navigation inside

--- PennySaver/PennySaver/Features/View/TabViews/GraphsView.swift
+++ PennySaver/PennySaver/Features/View/TabViews/GraphsView.swift
@@ -14,16 +14,40 @@ struct GraphsView: View {
     @Query(animation: .snappy) private var transactions: [Transaction]
     @State private var chartGroups: [ChartGroup] = []
     var body: some View {
-        ScrollView(.vertical) {
-            LazyVStack(spacing: 10) {
-                ChartView()
-                    .padding(10)
-                    .frame(height: 200)
-                    .background(.background, in: .rect(cornerRadius: 10))
+        NavigationStack {
+            ScrollView(.vertical) {
+                LazyVStack(spacing: 10) {
+                    ChartView()
+                        .frame(height: 200)
+                        .padding(10)
+                        .padding(.top, 10)
+                        .background(.background, in: .rect(cornerRadius: 10))
+                        .opacity(transactions.isEmpty ? 0 : 1)
+
+                    ForEach(chartGroups) { group in
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text(format(date: group.date, format: "MMM yy"))
+                                .font(.caption)
+                                .foregroundStyle(.gray)
+                                .hSpacing(.leading)
+
+                            NavigationLink {
+                                ListOfExpenses(month: group.date)
+                            } label: {
+                                CardView(income: group.totalIncome, expense: group.totalExpense)
+                            }
+                            .buttonStyle(.plain)
+                        }
+                    }
+                }
+                .padding(15)
             }
-        }
-        .onAppear {
-            // Creating chart group -> update data when appears.
+            .navigationTitle("Dados")
+            .background(.gray.opacity(0.15))
+            .onAppear {
+                // Creating chart group -> update data when appears.
+                createChartGroup()
+            }
         }
     }
 
@@ -43,6 +67,19 @@ struct GraphsView: View {
             }
         }
         .chartScrollableAxes(.horizontal)
+        .chartXVisibleDomain(length: 4)
+        .chartLegend(position: .bottom, alignment: .trailing)
+        .chartYAxis {
+            AxisMarks(position: .leading) { value in
+                let doubleValue = value.as(Double.self) ?? 0
+
+                AxisGridLine()
+                AxisTick()
+                AxisValueLabel {
+                    Text(axisLabel(doubleValue))
+                }
+            }
+        }
         .chartForegroundStyleScale(range: [Color.green.gradient, Color.red.gradient])
     }
 
@@ -63,7 +100,87 @@ struct GraphsView: View {
 
                 return calendar.compare(date1, to: date2, toGranularity: .day) == .orderedDescending
             }
+
+            let chartGroups = sortedGroups.compactMap { dict -> ChartGroup? in
+                let date = calendar.date(from: dict.key) ?? .init()
+                let income = dict.value.filter({ $0.category == Category.income.rawValue })
+                let expense = dict.value.filter({ $0.category == Category.expense.rawValue })
+
+                let incomeTotalValue = total(income, category: .income)
+                let expenseTotalValue = total(expense, category: .expense)
+
+                return .init(
+                    date: date,
+                    categories: [
+                        .init(totalValue: incomeTotalValue, category: .income),
+                        .init(totalValue: expenseTotalValue, category: .expense)
+                    ],
+                    totalIncome: incomeTotalValue,
+                    totalExpense: expenseTotalValue
+                )
+            }
+
+            // UI must be updated on main thread.
+            await MainActor.run {
+                self.chartGroups = chartGroups
+            }
         }
+    }
+
+    func axisLabel(_ value: Double) -> String {
+        let intValue = Int(value)
+        let kValue = intValue / 1000
+
+        return intValue < 1000 ? "\(intValue)" : "\(kValue)K"
+    }
+}
+
+// List Of Transactions for the Selected Month
+struct ListOfExpenses: View {
+    let month: Date
+    var body: some View {
+        ScrollView(.vertical) {
+            LazyVStack(spacing: 15) {
+                Section {
+                    FilterTransactionView(startDate: month.startOfMonth, endDate: month.endOfMonth, category: .income) { transactions in
+                        ForEach(transactions) { transaction in
+                            NavigationLink {
+                                TransactionView(editTransaction: transaction)
+                            } label: {
+                                TransactionCardView(transaction: transaction)
+                            }
+                            .buttonStyle(.plain)
+                        }
+                    }
+                } header: {
+                    Text("Renda")
+                        .font(.caption)
+                        .foregroundStyle(.gray)
+                        .hSpacing(.leading)
+                }
+
+                Section {
+                    FilterTransactionView(startDate: month.startOfMonth, endDate: month.endOfMonth, category: .expense) { transactions in
+                        ForEach(transactions) { transaction in
+                            NavigationLink {
+                                TransactionView(editTransaction: transaction)
+                            } label: {
+                                TransactionCardView(transaction: transaction)
+                            }
+                            .buttonStyle(.plain)
+                        }
+                    }
+                } header: {
+                    Text("Despesa")
+                        .font(.caption)
+                        .foregroundStyle(.gray)
+                        .hSpacing(.leading)
+                }
+            }
+            .padding(15)
+        }
+        .background(.gray.opacity(0.15))
+        .navigationTitle(format(date: month, format: "MMM yy"))
     }
 }
 
