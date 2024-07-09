079972df0712c492c00944ed04ccc6d35aec383e
Lucas Neves <lucas.pompeu02@hotmail.com>
2024-07-09 19:14:40 -0300

    DataManager + folders organization.

---
+++ PennySaver/PennySaver/DataManager/DataManager.swift
@@ -0,0 +1,14 @@
+//
+//  DataManager.swift
+//  PennySaver
+//
+//  Created by Lucas Neves dos santos pompeu on 08/07/24.
+//
+
+import Foundation
+
+final class DataManager: NSObject, ObservableObject {
+    
+}
+
+
